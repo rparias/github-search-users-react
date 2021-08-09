ef46d5cc3ea3b28da3b5f3aa9d05e263a0798905
Ronald Arias <ariasron@hotmail.com>
2021-08-09 12:37:02 -0500

    Gets the most popular languages from repos and passes into the Pie3D component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -2,33 +2,41 @@ import React from "react";
 import styled from "styled-components";
 import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
+
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
-  console.log(repos);
 
-  const chartData = [
-    {
-      label: "HTML",
-      value: "13",
-    },
-    {
-      label: "CSS",
-      value: "23",
-    },
-    {
-      label: "JavaScript",
-      value: "80",
-    },
-  ];
+  const languages = getMostPopularLanguages(repos);
 
   return (
     <section className="section">
       <Wrapper className="section-center">
-        {/* <ExampleChart data={chartData} /> */}
-        <Pie3D data={chartData} />
+        <Pie3D data={languages} />
       </Wrapper>
     </section>
   );
+};
+
+const getMostPopularLanguages = (repos) => {
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+
+    if (!language) return total;
+
+    total[language] = !total[language]
+      ? { label: language, value: 1 }
+      : { ...total[language], value: total[language].value + 1 };
+
+    return total;
+  }, {});
+
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  return languages;
 };
 
 const Wrapper = styled.div`
