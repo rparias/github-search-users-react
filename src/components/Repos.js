94ae7063191d658e38417ba2bd96b8aadb59a455
Ronald Arias <ariasron@hotmail.com>
2021-08-09 13:11:07 -0500

    Display the most popular and most used languages

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -6,14 +6,15 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
 
-  const languages = getMostPopularLanguages(repos);
+  const { mostUsedLanguages, mostPopularLanguages } =
+    getMostPopularLanguages(repos);
 
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <Pie3D data={languages} />
+        <Pie3D data={mostUsedLanguages} />
         <div></div>
-        <Doughnut2D data={languages} />
+        <Doughnut2D data={mostPopularLanguages} />
         <div></div>
       </Wrapper>
     </section>
@@ -21,25 +22,38 @@ const Repos = () => {
 };
 
 const getMostPopularLanguages = (repos) => {
-  let languages = repos.reduce((total, item) => {
-    const { language } = item;
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
 
     if (!language) return total;
 
     total[language] = !total[language]
-      ? { label: language, value: 1 }
-      : { ...total[language], value: total[language].value + 1 };
+      ? { label: language, value: 1, stars: stargazers_count }
+      : {
+          ...total[language],
+          value: total[language].value + 1,
+          stars: total[language].stars + stargazers_count,
+        };
 
     return total;
   }, {});
 
-  languages = Object.values(languages)
+  const mostUsedLanguages = Object.values(languages)
     .sort((a, b) => {
       return b.value - a.value;
     })
     .slice(0, 5);
 
-  return languages;
+  const mostPopularLanguages = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
+
+  return { mostUsedLanguages, mostPopularLanguages };
 };
 
 const Wrapper = styled.div`
