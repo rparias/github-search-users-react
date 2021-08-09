8d29cc52e3c0bb955d8a2265e0b2b6ea1ad3cf82
Ronald Arias <ariasron@hotmail.com>
2021-08-09 11:46:51 -0500

    Passing data from Repos component to ExmapleChart and refactors code

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,7 +5,29 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
   console.log(repos);
-  return <ExampleChart />;
+
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13",
+    },
+    {
+      label: "CSS",
+      value: "23",
+    },
+    {
+      label: "JavaScript",
+      value: "80",
+    },
+  ];
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <ExampleChart data={chartData} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
