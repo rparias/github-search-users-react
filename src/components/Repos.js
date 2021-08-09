cd4819dd699850d5c469be721c9dc4daafc95ffe
Ronald Arias <ariasron@hotmail.com>
2021-08-09 12:54:09 -0500

    Displays doughnut2d component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -12,6 +12,9 @@ const Repos = () => {
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={languages} />
+        <div></div>
+        <Doughnut2D data={languages} />
+        <div></div>
       </Wrapper>
     </section>
   );
